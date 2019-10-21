02c4ae1524bde0640ca9439718d4c6dea6433b08
Arthur Detant <arthur.detant1996@gmail.com>
2019-10-21 14:04:17 +0200

    brouillon

---
+++ app/src/main/java/com/example/magicapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.magicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4players);
+    }
+}
